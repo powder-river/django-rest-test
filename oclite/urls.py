08e087e3126be92e83816735c7900d25d99f4202
Danai Adkisson <danai@powder-river.io>
2017-08-30 21:00:18 -0400

    tutorial

--- oclite/urls.py
+++ oclite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('snippets.urls')),
 ]
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
