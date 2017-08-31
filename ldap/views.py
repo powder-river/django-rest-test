0da8b0ee93432816961245cd7fbea8dc00978235
Danai Adkisson <da129@duke.edu>
2017-08-31 10:52:05 -0400

    (master) working with CORS

--- ldap/views.py
+++ ldap/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from ldap.ldap import DukeLdap
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # Create your views here.
+# @csrf_exempt
 class SearchLdap(APIView):
     def get(request,format=None):
         ldap =  DukeLdap()
--- oclite/settings.py
+++ oclite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework', #django rest framework library
     'ldap3', #ldap library
     'snippets.apps.SnippetsConfig', #snippets app
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
